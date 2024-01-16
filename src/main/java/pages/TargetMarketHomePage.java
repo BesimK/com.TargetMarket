f891626150c5c53394669f530039ebcae84033ae
Selcuk KARAKAS <selcukkarakas1@gmail.com>
2024-01-16 18:56:32 +0300

    1 page setUP done

--- src/main/java/pages/TargetMarketHomePage.java
+++ src/main/java/pages/TargetMarketHomePage.java
@@ -9,47 +9,57 @@ import java.util.List;
 
 public class TargetMarketHomePage extends BasePage {
 
-	@FindBy(xpath = "//h5[@class=\"display-5\"]")
-	private WebElement welcomeText;
-	@FindBy(xpath = "//button[text()=\"Logout\"]")
-	private WebElement logoutButton;
-	@FindBy(xpath = "(//ul[@id=\"splide02-list\"]/li)")
-	private List<WebElement> tabs;
-	@FindBy(xpath = "(//ul[@id=\"splide02-list\"])")
-	private WebElement slideList;
-	@FindBy(xpath = "//span[@class=\"cart-badge m-0 mt-1 badge rounded-pill bg-danger\"]")
-	private WebElement cartCounter;
-	@FindBy(xpath = "//button[text()=\"Go to Checkout\"]")
-	private WebElement goToCheckoutButton;
-	@FindBy(xpath = "//button[@aria-label=\"Close\"]")
-	private WebElement closeDialogButton;
-	@FindBy(xpath = "(//h1)[1]")
-	private WebElement checkOutMessage;
+    @FindBy(xpath = "//h5[@class=\"display-5\"]")
+    private WebElement welcomeText;
+    @FindBy(xpath = "//button[text()=\"Logout\"]")
+    private WebElement logoutButton;
+    @FindBy(xpath = "(//ul[@id=\"splide02-list\"]/li)")
+    private List<WebElement> tabs;
+    @FindBy(xpath = "(//ul[@id=\"splide02-list\"])")
+    private WebElement slideList;
+    @FindBy(xpath = "//span[@class=\"cart-badge m-0 mt-1 badge rounded-pill bg-danger\"]")
+    private WebElement cartCounter;
+    @FindBy(xpath = "//button[text()=\"Go to Checkout\"]")
+    private WebElement goToCheckoutButton;
+    @FindBy(xpath = "//button[@aria-label=\"Close\"]")
+    private WebElement closeDialogButton;
+    @FindBy(xpath = "(//h1)[1]")
+    private WebElement checkOutMessage;
 
-	public void clickLogoutButton() {
-		logoutButton.click();
-	}
+    public void clickLogoutButton() {
+        logoutButton.click();
+    }
 
-	public void swipeTabsLeft(){
-		JSUtils.executeJS("arguments[0].setAttribute('style','transform: translateX(-285px);')", slideList);
-		BrowserUtils.wait(2.0);
-	}
-	public void clickWomenDresses(){
-		tabs.get(8).click();
-	}
-	public String getTextFromCartCounter(){
-		return cartCounter.getText();
-	}
-	public void clickCartButton(){
-		cartCounter.click();
-	}
-	public void clickGoToCheckoutButton(){
-		goToCheckoutButton.click();
-	}
-	public String getCheckoutMessage(){
-		return checkOutMessage.getText();
-	}
-	public void clickCloseDialogButton(){
-		closeDialogButton.click();
-	}
+    public void swipeTabsLeft() {
+        JSUtils.executeJS("arguments[0].setAttribute('style','transform: translateX(-285px);')", slideList);
+        BrowserUtils.wait(2.0);
+    }
+
+    public void clickWomenDresses() {
+        tabs.get(8).click();
+    }
+
+    public void clickHomeDecoration() {
+        tabs.get(5).click();
+    }
+
+    public String getTextFromCartCounter() {
+        return cartCounter.getText();
+    }
+
+    public void clickCartButton() {
+        cartCounter.click();
+    }
+
+    public void clickGoToCheckoutButton() {
+        goToCheckoutButton.click();
+    }
+
+    public String getCheckoutMessage() {
+        return checkOutMessage.getText();
+    }
+
+    public void clickCloseDialogButton() {
+        closeDialogButton.click();
+    }
 }
