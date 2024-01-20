c158cfc44712c2ba85bd96ae67946bfeffb42fe0
serteser <serteser@inar-academy.com>
2024-01-20 04:01:24 +0200

    adding Laptops Test Cases By Gurkan

--- src/main/java/utils/Pages.java
+++ src/main/java/utils/Pages.java
@@ -6,14 +6,20 @@ public class Pages {
 
 	private HomePage homePage;
 
+	private LaptopsPage laptopsPage;
+
 	private SmartphonesPage smartphonesPage;
 
 	private TargetMarketLoginPage targetMarketLoginPage;
 
 	private TargetMarketHomePage targetMarketHomePage;
+
 	private WomenDressesPage womenDressesPage;
+
 	private CheckoutPage checkoutPage;
+
 	private TopsPage topsPage;
+
 	private FurniturePage furniturePage;
 
 	private GroceriesPage groceriesPage;
@@ -30,12 +36,10 @@ public class Pages {
 		checkoutPage = new CheckoutPage();
 		topsPage = new TopsPage();
 		smartphonesPage = new SmartphonesPage();
-
 		skincarePage = new SkincarePage();
 		groceriesPage = new GroceriesPage();
-
 		furniturePage = new FurniturePage();
-
+		laptopsPage = new LaptopsPage();
 	}
 
 	public HomePage getHomePage() {
@@ -53,12 +57,15 @@ public class Pages {
 	public WomenDressesPage getWomenDressesPage() {
 		return womenDressesPage;
 	}
+
 	public CheckoutPage getCheckoutPage(){
 		return checkoutPage;
 	}
+
 	public TopsPage getTopsPage(){
 		return topsPage;
 	}
+
 	public SmartphonesPage getSmartphonesPage(){
 		return smartphonesPage;
 	}
@@ -72,4 +79,6 @@ public class Pages {
 	}
   
 	public FurniturePage getFurniturePage(){return  furniturePage;}
+
+	public LaptopsPage getLaptopsPage(){return  laptopsPage;}
 }
---
+++ src/test/java/LaptopsTests/TC_023_LP_002.java
@@ -0,0 +1,50 @@
+package LaptopsTests;
+
+import BaseTest.Hooks;
+import org.testng.annotations.Test;
+import utils.BrowserUtils;
+
+/**
+ * 1. Open the URL.
+ * 2. Navigate to Target Market Home Page.
+ * 3. Click on the "Laptops" button.
+ * 4. Click on the "Microsoft Surface Laptop 4" Add to Cart button.
+ * 5. Verify that the product is added in shopping cart icon.
+ * 6. Click on the shopping cart icon.
+ * 7. Verify that the product is added in shopping cart correctly.
+ * 8. Click the '+' button twice for 'Microsoft Surface Laptop 4'.
+ * 9. Verify that the total price is changed correctly.
+ */
+public class TC_023_LP_002 extends Hooks {
+
+    @Test
+    void verifyWhetherCountsOfProductsDisplayedInShoppingCartCorrect(){
+
+        // Open the URL and Navigate to Target Market Home Page.
+
+        // Click on the "Laptops" button.
+        pages.getTargetMarketHomePage().clickTab(2);
+
+        // Click on the "Microsoft Surface Laptop 4" Add to Cart button.
+        BrowserUtils.scrollDownWithPageDown();
+        pages.getLaptopsPage().clickOnSelectedProduct("Microsoft Surface Laptop 4");
+
+        // Verify that the product is added in shopping cart icon.
+        String countOfProductsInShoppingCart = pages.getLaptopsPage().getCountOfShoppingCart();
+        softAssert.assertEquals(countOfProductsInShoppingCart, "1", "Counts of product should be 1");
+
+        // Click on the shopping cart icon.
+        pages.getLaptopsPage().clickOnShoppingCartIcon();
+
+        // Verify that the product is added in shopping cart correctly.
+        boolean areTheseProductsAddedToShoppingCart = pages.getLaptopsPage().areTheseProductsAddedToShoppingCart("Microsoft Surface Laptop 4");
+        softAssert.assertTrue(areTheseProductsAddedToShoppingCart, "There should be only one product in the shopping cart but there is not");
+
+        // Click the '+' button twice for 'Microsoft Surface Laptop 4'.
+        pages.getLaptopsPage().pressPlusMinusSignInShoppingCart(1);
+
+        // Verify that the total price is changed correctly.
+        boolean isTotalPriceInShoppingCartCorrect = pages.getLaptopsPage().isTotalPriceInShoppingCartCorrect();
+        softAssert.assertTrue(isTotalPriceInShoppingCartCorrect, "Total price in shopping cart does not match with the total price of product(s)");
+    }
+}
